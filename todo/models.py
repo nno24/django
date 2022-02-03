cd729e1154af52b41977016fdaadc9e2398d1a68
nno24 <nicolai.norseng@gmail.com>
2022-02-03 14:23:38 +0000

    Added how to render elements form the database to the frontend

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#item is like the sheet in excel, class inheritance models.Model
+
+
+class Item(models.Model): 
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+#Override the name from the generic class 
+    def __str__(self):
+        return self.name
+        
